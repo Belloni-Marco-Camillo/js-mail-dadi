f987a11b3d6f62d264967b60a4c5bfbdd19024db
Belloni-Marco-Camillo <bellonimarcocamillo@gmail.com>
2022-03-29 16:26:35 +0200

    final-commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,26 +6,49 @@ const emails = [`marco@mail.com`, `giuseppe@mail.com`, `simone@mail.com`]
 function myFunction() {
     //inseriamo la mail dal HTML
     const insertEmail = document.getElementById(`insertEmail`).value;
+
+    let vero_falso = false;
     //prendiamo in esame tutte le mail
     for (let i = 0; i < emails.length; i++) {
+        //assegno ad una variabile la mail per ogni 
         let email = emails[i]
         if (insertEmail == email) {
-            document.getElementById("enter").innerHTML =
-                "entra nel gioco";
+            vero_falso = true;
         }
     }
-}
 
-function Lancio() {
-    let lancioMio = document.getElementById(`lancioMio`).value
-    console.log(lancioMio);
-    let lancioComp = Math.floor(Math.random() * 6) + 1
-    if (lancioMio == lancioComp) {
-        alert `pareggio`
-    } else if (lancioMio > lancioComp) {
-        alert `hai vinto`
+    console.log(vero_falso);
+    if (vero_falso == true) {
+        document.getElementById("enter").innerHTML =
+            "entra nel gioco";
     } else {
-        alert `hai perso`
+        document.getElementById("reset").innerHTML =
+            "inserisci nuovamente la email";
     }
-    console.log(lancioComp);
+}
+let vittoria_sconfitta = false
+
+
+//funzione del lancio
+function Lancio() {
+    //inserisco il mio lancio in una variabile
+    let lancioMio = document.getElementById(`lancioMio`).value
+        //inserisco una variabile di un numero casuale
+    let lancioComp = Math.floor(Math.random() * 6) + 1
+        //se il mio lancio è maggiore della variabile del compuer
+    if (lancioMio > lancioComp) {
+        //creo un messaggio di vittoria
+        document.getElementById("risultato").innerHTML = `hai vinto`
+            //se il mio lancio è inferiore alla variabile del computer
+    } else if (lancioMio < lancioComp) {
+        //creo un messaggio di perdita
+        document.getElementById("risultato").innerHTML = `hai perso`
+    } else {
+        //creo un messaggio di pareggio
+        document.getElementById("risultato").innerHTML = `questo è un pareggio`
+    }
+    //mostro a schermo il mio lancio
+    document.getElementById("myLancio").innerHTML = `il tuo lancio è ${lancioMio}`;
+    //mostro a schermo il numero casuale del computer
+    document.getElementById("randomNumber").innerHTML = `il lacio del computer è ${lancioComp}`;
 }
